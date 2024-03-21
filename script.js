2566c4cd9d0b9656d1ecd6a6ced95d551bd422d7
Bibi-owoeye <owoeyebibi1@gmail.com>
2024-03-21 02:52:19 -0700

    some changes

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var cart = []
 var show = document.getElementById('show');
 var placeHolder = document.getElementById('placeholder');
 var allButtons= document.getElementById('allButtons');
+var delAnyItems = document.getElementById('delAnyItems');
+var delAnyItems2 = document.getElementById('delAnyItems2');
 
 
 
@@ -22,8 +24,7 @@ function addItems(){
         noItem.style.display = 'none'
         cart.push(inputItem.value)
         console.log(cart);
-        document.getElementById('inputItem').value= ('')
-        show.innerHTML= ''
+        document.getElementById('inputItem').value= ''
         displayItem()
     }
 }
@@ -58,16 +59,44 @@ function deleteLast(){
     displayItem()
 }
 function addItemFirst(){
-    cart.unshift(document.getElementById('inputItem').value)
-    displayItem()
+    if (firstItem.value == ''){
+        noItemAdded.style.display = 'block'
+
+    
+}else{
+        
+        noItemAdded.style.display = 'none'
+        cart.unshift(firstItem.value)
+        console.log(cart);
+        document.getElementById('firstItem').value= ('')
+        displayItem()    }
 }
 function deleteAnyItem(){
-    var start = Number(prompt('Which number are you deleting'))
-    if( start > cart.length){
-        alert('maximum number execeeded')
-    }
-    cart.splice(start-1,1)
-    displayItem()
+    // var start = Number(prompt('Which number are you deleting'))
+    // if( start > cart.length){
+    //     alert('maximum number execeeded')
+    // }
+    // cart.splice(start-1,1)
+    // displayItem()
+    if (deleteAny.value == ''){
+        delAnyItems2.style.display='none' 
+        delAnyItems.style.display='block'   }
+        else{
+
+            if (Number(deleteAny.value > cart.length)){
+                delAnyItems2.style.display='block' 
+                delAnyItems.style.display='none' 
+                // alert('maximum number excedeed')
+            }else{
+                // exampleModal1.style.display='none'
+                delAnyItems.style.display = 'none'
+                delAnyItems2.style.display='none' 
+                var newIndex= Number(document.getElementById('deleteAny').value)
+                cart.splice(newIndex-1 , 1)
+                deleteAny.value=''
+                displayItem()
+            }
+        }
 }
 function deleteAllItem(){
     var confirmation =confirm('Are you sure you want to delete')
@@ -76,8 +105,26 @@ function deleteAllItem(){
     displayItem()
 }
 function editAnyItem(){
-    var editAny = Number(prompt('Which number do you want to Edit'))
-    var replacement = prompt('new value')
-    cart.splice(editAny-1 , 1,replacement)
-    displayItem()
+    if (newNumber.value == ''){
+        alert('Please input a number')    }
+    if (Number(newNumber.value > cart.length)){
+        alert('maximum number excedeed')
+    }else{
+        // exampleModal1.style.display='none'
+        var newIndex= Number(document.getElementById('newNumber').value)
+        var replacement= document.getElementById('newValue').value
+        cart.splice(newIndex-1 , 1,replacement)
+        newNumber.value= ''
+        newValue.value = ''
+
+        displayItem()
+    }
+
+
+
+
+    // var editAny = Number(prompt('Which number do you want to Edit'))
+    // var replacement = prompt('new value')
+    // cart.splice(editAny-1 , 1,replacement)
+    // displayItem()
 }
